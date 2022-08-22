c4d33586b8a38bcd07a87b3634e649242f5657aa
zhaozhifeng <616089013@qq.com>
2022-08-22 22:50:31 +0800

    hot23. 合并K个升序链表

---
+++ algorithms/java/src/hot100/hot23.java
@@ -0,0 +1,49 @@
+package hot100;
+
+import java.util.PriorityQueue;
+
+/**
+ * 23. 合并K个升序链表
+ *
+ * @author zzf
+ * @date 2022/8/22 10:16 下午
+ */
+public class hot23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>((o1, o2) -> (o1.val - o2.val));
+        for (ListNode list : lists) {
+            if (list != null) {
+                queue.add(list);
+            }
+        }
+        ListNode ans = new ListNode();
+        ListNode remove = ans;
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            remove.next = node;
+            if (node.next != null) {
+                queue.add(node.next);
+            }
+            remove = remove.next;
+        }
+        return ans.next;
+    }
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>((o1, o2) -> (o1.val - o2.val));
+        for (ListNode list : lists) {
+            while (list != null) {
+                queue.add(list);
+                list = list.next;
+            }
+        }
+        ListNode ans = new ListNode();
+        ListNode remove = ans;
+        while (!queue.isEmpty()) {
+            remove.next = queue.poll();
+            remove = remove.next;
+        }
+        remove.next = null;
+        return ans.next;
+    }
+}
