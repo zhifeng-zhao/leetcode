f133b75657b6649f5473a92e04059167ca2863e6
zhaozhifeng <616089013@qq.com>
2022-08-29 23:43:59 +0800

    hot01.两数之和

--- algorithms/java/src/hot100/hot01.java
+++ algorithms/java/src/hot100/hot01.java
@@ -1,12 +1,23 @@
 package hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 1.两数之和
+ *
  * @author zzf
  * @date 2022/8/18 12:38 下午
  */
 public class hot01 {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
         return new int[0];
     }
 }
