31ba592e16fa15926494af8ef98bd529b7ad50f2
zhaozhifeng <616089013@qq.com>
2022-09-09 02:56:26 +0800

    hot41. 缺失的第一个正数

---
+++ algorithms/java/src/hot100/hot41.java
@@ -0,0 +1,29 @@
+package hot100;
+
+/**
+ * 41. 缺失的第一个正数
+ * @author zzf
+ * @date 2022/9/9 2:47 上午
+ */
+public class hot41 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
