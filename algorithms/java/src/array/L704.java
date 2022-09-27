01063b90fb520c134bdaff9efa38246df0e21e44
zhaozhifeng <616089013@qq.com>
2022-09-27 18:25:22 +0800

    L704. 二分查找

---
+++ algorithms/java/src/array/L704.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * 二分查找
+ * @author zzf
+ * @date 2022/9/27 5:37 下午
+ */
+public class L704 {
+    public int search(int[] nums, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
