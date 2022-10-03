74bbc56fa3d9dc921954ad0f1bc54cf0d0e77893
zhaozhifeng <616089013@qq.com>
2022-10-04 00:25:24 +0800

    hot18. 四数之和

--- algorithms/java/src/hot100/hot18.java
+++ algorithms/java/src/hot100/hot18.java
@@ -93,6 +93,51 @@ public class hot18 {
         return ans;
     }
 
+    /**
+     * 双指针
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum3(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i] > target) {
+                return res;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum > target) {
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         new hot18().fourSum2(new int[]{0, 0, 0, 1000000000, 1000000000, 1000000000, 1000000000}, 1000000000);
     }
