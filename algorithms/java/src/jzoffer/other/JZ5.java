e4c708f487e4d9334af65ffb4f69c3cceb033eb5
zhaozhifeng <616089013@qq.com>
2022-10-04 16:30:12 +0800

    JZ5. 替换空格

--- algorithms/java/src/jzoffer/other/JZ5.java
+++ algorithms/java/src/jzoffer/other/JZ5.java
@@ -18,4 +18,35 @@ public class JZ5 {
         }
         return str.toString();
     }
+
+    public String replaceSpace2(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        StringBuffer str = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                str.append("  ");
+            }
+        }
+        if (str.length() == 0) {
+            return s;
+        }
+        int left = s.length() - 1;
+        s += str;
+        int right = s.length() - 1;
+        char[] chars = s.toCharArray();
+        while (left >= 0) {
+            if (chars[left] == ' ') {
+                chars[right--] = '0';
+                chars[right--] = '2';
+                chars[right] = '%';
+            } else {
+                chars[right] = chars[left];
+            }
+            right--;
+            left--;
+        }
+        return new String(chars);
+    }
 }
