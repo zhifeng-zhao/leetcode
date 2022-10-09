fcb4a5af0455277f72f0c351e87b2149131322b0
zhaozhifeng <616089013@qq.com>
2022-10-09 15:56:24 +0800

    L110. 平衡二叉树

---
+++ algorithms/java/src/tree/L110.java
@@ -0,0 +1,31 @@
+package tree;
+
+/**
+ * 110. 平衡二叉树
+ *
+ * @author zzf
+ * @date 2022/10/9 3:45 下午
+ */
+public class L110 {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = getHeight(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
