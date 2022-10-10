b9bb4856e0590380526bc4595cb9e0678fc38260
zhaozhifeng <616089013@qq.com>
2022-10-10 13:38:39 +0800

    L617. 合并二叉树

---
+++ algorithms/java/src/tree/L617.java
@@ -0,0 +1,21 @@
+package tree;
+
+/**
+ * 617. 合并二叉树
+ * @author zzf
+ * @date 2022/10/10 1:34 下午
+ */
+public class L617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+}
